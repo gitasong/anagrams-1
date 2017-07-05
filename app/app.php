2935db55f2b768190a0b3339244ed00bcc586d1b
Larry Taylor and Max Scher <larry@>
2017-07-05 13:26:34 -0700

    build initial links in app and index

---
+++ app/app.php
@@ -0,0 +1,11 @@
+<?php
+    date_default_timezone_set('America/Los_Angeles');
+    require_once __DIR__.'/../vendor/autoload.php';
+    require_once __DIR__.'/../src/Anagram.php';
+
+    $app = new Silex\Application();
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
+
+    $app->get('/', function() use ($app) {
+
+?>
