36401f37c07d6516d9c8c0a433566f20b5ea485b
Larry Taylor and Max Scher <max@>
2017-07-05 16:14:21 -0700

    refactor checkAnagram() () to use arrays instead of strings

--- src/Anagram.php
+++ src/Anagram.php
@@ -2,11 +2,31 @@
 
     class Anagram
     {
-
-        function checkAnagram($input)
+        function checkAnagram($main_word, $input_0, $input_1, $input_2)
         {
-            
+            $result_arr = [$input_0 => $input_0, $input_1 => $input_1, $input_2 => $input_2];
+            $anagram_arr = [$input_0 => $input_0, $input_1 => $input_1, $input_2 => $input_2];
+
+            $main_word_explode = str_split($main_word);
+            sort($main_word_explode);
+            $main_word_alph = implode($main_word_explode);
+
+            foreach ($anagram_arr as $value)
+            {
+                $new_value = str_split($value);
+                sort($new_value);
+                $value = implode("", $new_value);
+            }
+
+            foreach ($anagram_arr as $key => $value)
+            {
+                if ($value == $main_word_alph) {
+                    $result_arr[$key] = true;
+                } else {
+                    $result_arr[$key] = false;
+                }
+            }
+            return $result_arr;
         }
     }
-
 ?>
---
+++ tests/AnagramTest.php
@@ -0,0 +1,36 @@
+<?php
+
+    require_once "src/Anagram.php";
+
+    class AnagramTest extends PHPUnit_Framework_TestCase
+    {
+        function testCheckAnagramOneLetterVsOneLetter()
+        {
+            // Arrange
+            $test_anagram = new Anagram;
+            $main_word = "A";
+            $input_0 = "T";
+            $input_1 = "";
+            $input_2 = "";
+
+            // Act
+            $result = $test_anagram->checkAnagram($main_word, $input_0, $input_1, $input_2);
+
+            // Assert
+            $this->assertEquals([$input_0 => false, $input_1 => false, $input_2 => false], $result);
+        }
+        // function testCheckAnagramMultiLetter()
+        // {
+        //     // Arrange
+        //     $test_anagram = new Anagram;
+        //     $main_word = "ON";
+        //     $input_0 = "NO";
+        //
+        //     // Act
+        //     $result = $test_anagram->checkAnagram($main_word, $input_0);
+        //
+        //     // Assert
+        //     $this->assertEquals([$input_0 => true], $result);
+        // }
+    }
+?>
