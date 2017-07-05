61dd64a43d996980dc3f5d4c0c435692efdb8814
Larry Taylor and Max Scher <max@>
2017-07-05 16:16:44 -0700

    pass 2nd test for two-letter anagrams

--- tests/AnagramTest.php
+++ tests/AnagramTest.php
@@ -19,18 +19,20 @@
             // Assert
             $this->assertEquals([$input_0 => false, $input_1 => false, $input_2 => false], $result);
         }
-        // function testCheckAnagramMultiLetter()
-        // {
-        //     // Arrange
-        //     $test_anagram = new Anagram;
-        //     $main_word = "ON";
-        //     $input_0 = "NO";
-        //
-        //     // Act
-        //     $result = $test_anagram->checkAnagram($main_word, $input_0);
-        //
-        //     // Assert
-        //     $this->assertEquals([$input_0 => true], $result);
-        // }
+        function testCheckAnagramMultiLetter()
+        {
+            // Arrange
+            $test_anagram = new Anagram;
+            $main_word = "ON";
+            $input_0 = "NO";
+            $input_1 = "";
+            $input_2 = "";
+
+            // Act
+            $result = $test_anagram->checkAnagram($main_word, $input_0, $input_1, $input_2);
+
+            // Assert
+            $this->assertEquals([$input_0 => true, $input_1 => false, $input_2 => false], $result);
+        }
     }
 ?>
